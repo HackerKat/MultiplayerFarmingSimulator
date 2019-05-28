9938c333db650cadd09ddbdd2e0211a92521441b
Kat <kate.rizhakova@gmail.com>
2019-05-28 21:47:37 +0200

    Sprite class addeed

---
+++ MFS/MFS/Player.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MFS
+{
+    class Player
+    {
+        Sprite playerSprite;
+        Rectangle collisionRect;
+
+        public Player(Sprite playerSprite, Rectangle collisionRect)
+        {
+            playerSprite = this.playerSprite;
+            collisionRect = this.collisionRect;
+        }
+
+        public override void Update(GameTime gameTime, Rectangle clientBounds)
+        {
+            //bounds check
+            if (position.X < 0)
+                position.X = 0;
+            if (position.Y < 0)
+                position.Y = 0;
+            if (position.X > clientBounds.Width - frameSize.X)
+                position.X = clientBounds.Width - frameSize.X;
+            if (position.Y > clientBounds.Height - frameSize.Y)
+                position.Y = clientBounds.Height - frameSize.Y;
+
+            base.Update(gameTime, clientBounds);
+        }
+    }
+}
--- MFS/MFS/Sprite.cs
+++ MFS/MFS/Sprite.cs
@@ -7,98 +7,47 @@ namespace MFS {
     {
         private Texture2D textureImage;
         protected Point frameSize;
-        private Point currentFrame;
         protected Point sheetSize;
-        private int collisionOffset;
-        private int timeSinceLastFrame = 0;
-        private int millisecondsPerFrame;
-        const int defaultMillisecondsPerFrame = 100;
-        protected Vector2 speed;
         protected Vector2 position;
+        private int millisecondsPerFrame;
 
-        public abstract Vector2 direction
-        {
-            get;
-        }
-
-        public Rectangle collisionRect
-        {
-            get
-            {
-                return new Rectangle(
-                    (int)position.X + collisionOffset,
-                    (int)position.Y + collisionOffset,
-                    frameSize.X - (collisionOffset * 2),
-                    frameSize.Y - (collisionOffset * 2));
-            }
-        }
-
-        public Sprite (Texture2D textureImage, Vector2 position, Point frameSize, int collisionOffset, Point currentFrame, Point sheetSize, Vector2 speed) 
-            : this(textureImage, position, frameSize, collisionOffset, currentFrame, sheetSize, speed, defaultMillisecondsPerFrame)
+        //for non-animated sprites
+        public Sprite (Texture2D textureImage, Vector2 position, Point frameSize) 
+            : this(textureImage, position, frameSize, new Point(1, 1), 0)
         {
         }
 
-        public Sprite(Texture2D textureImage, Vector2 position, Point frameSize, int collisionOffset, Point currentFrame, Point sheetSize, Vector2 speed, int millisecondsPerFrame)
+        //for animated sprites
+        public Sprite(Texture2D textureImage, Vector2 position, Point frameSize, Point sheetSize, int millisecondsPerFrame)
         {
             this.textureImage = textureImage;
             this.position = position;
             this.frameSize = frameSize;
-            this.collisionOffset = collisionOffset;
             this.sheetSize = sheetSize;
-            this.speed = speed;
             this.millisecondsPerFrame = millisecondsPerFrame;
         }
 
-
-        public List<Texture2D> CropTiles()
+        public void Animate(GameTime gameTime)
         {
-            List<Texture2D> textureList = new List<Texture2D>();
-            int xSprite = 0;
-            int ySprite = 0;
-            Rectangle newBounds = new Rectangle(xSprite, ySprite, frameSize.X, frameSize.Y);
-
-            while (xSprite < textureImage.Width && ySprite < textureImage.Height)
+            if (sheetSize != new Point(1, 1))
             {
-                Texture2D croppedTexture = new Texture2D(textureImage.GraphicsDevice, frameSize.X, frameSize.Y);
-                Color[] data = new Color[frameSize.X * frameSize.Y];
-                textureImage.GetData(0, newBounds, data, 0, newBounds.Width * newBounds.Width);
-                croppedTexture.SetData(data);
-
-                textureList.Add(croppedTexture);
-
-                xSprite += frameSize.X;
-                ySprite += frameSize.Y;
-            }
-            return textureList;
-        }
-
-        public virtual void Update (GameTime gameTime, Rectangle clientBounds)
-        {
-            timeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
-            if (timeSinceLastFrame > millisecondsPerFrame)
-            {
-                timeSinceLastFrame = 0;
-                ++currentFrame.X;
-                if (currentFrame.X >= sheetSize.X)
+                int timeSinceLastFrame = 0;
+                Point currentFrame = new Point(0, 0);
+                timeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
+                if (timeSinceLastFrame > millisecondsPerFrame)
                 {
-                    currentFrame.X = 0;
-                    ++currentFrame.Y;
-                    if (currentFrame.Y >= sheetSize.Y)
-                        currentFrame.Y = 0;
+                    timeSinceLastFrame = 0;
+                    ++currentFrame.X;
+                    if (currentFrame.X >= sheetSize.X)
+                    {
+                        currentFrame.X = 0;
+                        ++currentFrame.Y;
+                        if (currentFrame.Y >= sheetSize.Y)
+                            currentFrame.Y = 0;
+                    }
                 }
             }
         }
 
-        public virtual void Draw (GameTime gameTime, SpriteBatch spriteBatch)
-        {
-                spriteBatch.Draw(textureImage, position,
-                new Rectangle(currentFrame.X * frameSize.X,
-                currentFrame.Y * frameSize.Y,
-                frameSize.X,
-                frameSize.Y),
-                Color.White, 0, Vector2.Zero,
-                1f, SpriteEffects.None, 0
-                );
-        }
     }
 }
